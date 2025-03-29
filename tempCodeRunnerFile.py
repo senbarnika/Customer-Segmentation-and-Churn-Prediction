b6fd1a77022decc48ac6192ae2f1395738ba2c5f
senbarnika <bsen_be22@thapar.edu>
2025-03-29 19:16:25 +0530

    shifting codebase to python notebook

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,5 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sb
-
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.cluster import KMeans
-
-import warnings
-warnings.filterwarnings('ignore')
-
-# Load the dataset
-df = pd.read_csv('new.csv')
-df.head()
+plt.subplots(figsize=(15, 10))
+for i, col in enumerate(objects):
+    plt.subplot(2, 2, i + 1)
+    sb.countplot(df[col])
+plt.show()
