c7fb4f07fceedd0f312c199d2aac0cb98936d97c
senbarnika <bsen_be22@thapar.edu>
2025-03-29 17:17:53 +0530

    Handled missing/null values

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ warnings.filterwarnings('ignore')
 # Load the dataset
 df = pd.read_csv('new.csv')
 print(df.head())
+df.shape
+
+#data preprocessing
+df.info()
+print (df.describe().T)
+#to check null values in datatset
+for col in df.columns:
+    temp = df[col].isnull().sum()
+    if temp > 0:
+        print(f'Column {col} contains {temp} null values.')
+        
+df = df.dropna()
+print("Total missing values are:", len(df))
+
+
+
